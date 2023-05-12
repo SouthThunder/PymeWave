46633fddb36c09faa9f9b96faaebfe6bdbcf391b
Daniel <castedaniel29@gmail.com>
2023-05-11 22:00:10 -0500

    Base de datos online

--- node/database/db.js
+++ node/database/db.js
@@ -1,14 +1,14 @@
 import {Sequelize} from 'sequelize';
 
 //const db = new Sequelize('nombre de la base de datos', 'usuario', 'contraseña, objeto el cual indica el host y la base de datos que se esta utilizando',
-const db = new Sequelize('PymeWave', 'root', '',{
-    host: 'localhost',
+const db = new Sequelize('bubfsppbrknjnvd8eeye', 'ujdagf6kfuum6tul', '10bIEVbm6k9uAkHocMmA',{
+    host: 'bubfsppbrknjnvd8eeye-mysql.services.clever-cloud.com',
     dialect: 'mysql',
     define: {
         timestamps: false,
         freezeTableName: true
     },
-    port: '3307' // este es el puerto que utiliza mi base de datos, se configura aca
+    port: '3306' // este es el puerto que utiliza mi base de datos, se configura aca
 });
 
 export default db;
