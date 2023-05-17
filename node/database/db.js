5a84191c415f33f32637e98e0eeed6b512ffbdb1
Davidfer <davidfernando1112@gmail.com>
2023-05-16 21:49:43 -0500

    correcion bd

--- node/database/db.js
+++ node/database/db.js
@@ -1,14 +1,14 @@
 import {Sequelize} from 'sequelize';
 
 //const db = new Sequelize('nombre de la base de datos', 'usuario', 'contraseña, objeto el cual indica el host y la base de datos que se esta utilizando',
-const db = new Sequelize('PymeWave', 'root', '',{
-    host: 'localhost',
+const db = new Sequelize('u507398501_PymeWave', 'u507398501_PymesWaves', 'Contraseña.123*',{
+    host: 'sql892.main-hosting.eu',
     dialect: 'mysql',
     define: {
         timestamps: false,
         freezeTableName: true
     },
-    port: '3306' // este es el puerto que utiliza mi base de datos, se configura aca
+    port: '%' // este es el puerto que utiliza mi base de datos, se configura aca
 });
 
 export default db;
